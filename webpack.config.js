f6bbea5975b23bd529786cff55bce61d74cdac49
Vincent <vincent.gon@orange.fr>
2016-09-25 17:45:12 +0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+var node_modules_dir = path.join(__dirname, 'node_modules');
+
+var config = {
+    entry: [
+        'webpack-dev-server/client?http://127.0.0.1:3000',
+        'webpack/hot/only-dev-server',
+        './app/Resources/js/app.js',
+    ],
+    output: {
+        path: path.join(__dirname, 'web/dist'),
+        filename: 'bundle.js',
+        publicPath: 'http://127.0.0.1:3000/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
