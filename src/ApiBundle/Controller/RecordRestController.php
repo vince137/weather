60ffa6aeb906ddbb7011da2f0a7d6894063d3d36
Gonzalez Vincent <v.gonzalez@sirene-holding.com>
2016-10-10 13:20:59 +0200

    Add records to the api

---
+++ src/ApiBundle/Controller/RecordRestController.php
@@ -0,0 +1,26 @@
+<?php
+namespace ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations\View;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Doctrine\ORM\Entity\Repository;
+
+use AppBundle\Entity\Record;
+
+class RecordRestController extends Controller
+{
+   /**
+     * @return array
+     * @View()
+    */
+    public function getRecordsAction()
+    {
+        $repository = $this->getDoctrine()->getManager()->getRepository('AppBundle:Record');
+
+        // Get all reports
+        $records = $repository->findBy(array(), array('date' => 'DESC'), 30);
+
+        return $records;
+    }
+}
