d334a6a3e7d785418ce95853b73dad3179b4935d
Gonzalez Vincent <v.gonzalez@sirene-holding.com>
2016-10-10 16:40:46 +0200

    [WIP] Record

--- app/Resources/jsx/RecordComponent.jsx
+++ app/Resources/jsx/RecordComponent.jsx
@@ -2,11 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 var RecordComponent = React.createClass({
-    reports: {},
+    records: {},
+
+    /**
+     * Get all Record from the api
+     * @return promise
+     */
+    getRecords: function () {
+        return fetch('http://wd.dev/web/app_dev.php/api/records')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                // Update the component data
+                this.records = responseJson;
+            }).catch((error) => {
+                console.error(error);
+            });
+    },
+
+    getTemperatureMin: function () {
+        'temperature_min', 
+    },
+
+    getTemperatureMax: function () {
+        'temperature_max', 
+    },
+
+    getHumidityMin: function () {
+        'humidity_min',
+
+    },
+
+    getHumidityMax: function () {
+        'humidity_max'
+    },
+
+
+    /**
+     * Loaded at the start
+     */
+    componentDidMount: function () {
+        this.getRecords();
+    },
 
     render: function () {
-        return(null);
-    } 
+        
+        let temperatureMin = this.getTemperatureMin();
+        let temperatureMax = this.getTemperatureMax();
+        let getHumidityMin = this.getHumidityMin();
+        let getHumidityMax = this.getHumidityMax();
+
+
+        return (null);
+    }
 
 });
 export default RecordComponent;
